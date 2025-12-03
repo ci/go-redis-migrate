9b1a9f0fb6d9d7e7843012e438f522462936ed94
Catalin <catalin.irimie@gmail.com>
2025-12-03 19:51:56 +0200

    feat: add cmd/copy TLS support

--- cmd/copy.go
+++ cmd/copy.go
@@ -1,19 +1,36 @@
 package cmd
 
 import (
+	"crypto/tls"
 	"fmt"
+	"log"
+	"sync"
+	"time"
+
 	"github.com/mediocregopher/radix/v3"
 	"github.com/obukhov/go-redis-migrate/src/pusher"
 	"github.com/obukhov/go-redis-migrate/src/reporter"
 	"github.com/obukhov/go-redis-migrate/src/scanner"
 	"github.com/spf13/cobra"
-	"log"
-	"sync"
-	"time"
 )
 
 var pattern string
 var scanCount, report, exportRoutines, pushRoutines int
+var sourceTLS, sourceTLSInsecure bool
+var destTLS, destTLSInsecure bool
+
+func createRedisClient(addr string, useTLS, insecureSkipVerify bool) (radix.Client, error) {
+	if useTLS {
+		tlsConfig := &tls.Config{
+			InsecureSkipVerify: insecureSkipVerify,
+		}
+		connFunc := func(network, addr string) (radix.Conn, error) {
+			return radix.Dial(network, addr, radix.DialUseTLS(tlsConfig))
+		}
+		return radix.NewPool("tcp", addr, 4, radix.PoolConnFunc(connFunc))
+	}
+	return radix.DefaultClientFunc("tcp", addr)
+}
 
 var copyCmd = &cobra.Command{
 	Use:   "copy <source> <destination>",
@@ -23,12 +40,12 @@ var copyCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Start copying")
 
-		clientSource, err := radix.DefaultClientFunc("tcp", args[0])
+		clientSource, err := createRedisClient(args[0], sourceTLS, sourceTLSInsecure)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		clientTarget, err := radix.DefaultClientFunc("tcp", args[1])
+		clientTarget, err := createRedisClient(args[1], destTLS, destTLSInsecure)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -69,4 +86,8 @@ func init() {
 	copyCmd.Flags().IntVar(&report, "report", 1, "Report current status every N seconds")
 	copyCmd.Flags().IntVar(&exportRoutines, "exportRoutines", 30, "Number of parallel export goroutines")
 	copyCmd.Flags().IntVar(&pushRoutines, "pushRoutines", 30, "Number of parallel push goroutines")
+	copyCmd.Flags().BoolVar(&sourceTLS, "source-tls", false, "Enable TLS for source Redis connection")
+	copyCmd.Flags().BoolVar(&sourceTLSInsecure, "source-tls-insecure", false, "Skip TLS certificate verification for source")
+	copyCmd.Flags().BoolVar(&destTLS, "dest-tls", false, "Enable TLS for destination Redis connection")
+	copyCmd.Flags().BoolVar(&destTLSInsecure, "dest-tls-insecure", false, "Skip TLS certificate verification for destination")
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/obukhov/go-redis-migrate
 go 1.15
 
 require (
-	github.com/mediocregopher/radix/v3 v3.2.0
+	github.com/BurntSushi/toml v1.5.0 // indirect
+	github.com/mediocregopher/radix/v3 v3.8.1
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/viper v1.3.1
